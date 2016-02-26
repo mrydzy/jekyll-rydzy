daf075d61faae4892a28d8c8a446692e925e019e
mrydzy <majrydzy@gmail.com>
2016-02-26 22:10:46 +0100

    tech map

---
+++ assets/js/map.js
@@ -0,0 +1,35 @@
+
+
+$('document').ready(function(){
+  var mapContainer = $('.map-container');
+//var categories = ['website', 'webapp', 'mobile']
+  var currentCategory;
+
+  $('.map-row').on('click', '.map-col', function(e){
+    var row = $(e.currentTarget).parent();
+    var target = e.currentTarget.dataset.target;
+    var category = e.currentTarget.dataset.category;
+    if (category != undefined) {
+      currentCategory = category;
+    }
+    var rowNumber = row.data('row');
+    removeChildren(rowNumber);
+    mapContainer.addClass('stage' + rowNumber + '-' + target + '-' + currentCategory);
+  });
+
+  var rowIdRegex = /\d+/;
+  function removeChildren(rowNumber) {
+    var classes = mapContainer[0].classList;
+    var classesNumber = classes.length;
+    for (var i = 0; i < classesNumber; i++) {
+      var rowid = rowIdRegex.exec(classes[i]);
+      if (rowid !== null) {
+        if (rowid[0] >= rowNumber) {
+          mapContainer.removeClass(classes[i]);
+          classesNumber--;
+          i--;
+        }
+      }
+    }
+  }
+});
---
+++ map.en.md
@@ -0,0 +1,7 @@
+---
+layout: map
+title: Hitchiker's guide through tech
+permalink: /map/
+lang: en
+---
+If you're here, most probably you've asked me which technology should you use for yet another world-changing solution you have designed. So here's the deal: I don't know all the technologies that are out there, but here's a bunch of them that can be a good starting point for the research.
